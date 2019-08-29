8e7e4c53e7a397c28f9802388591471229f9afb9
Daisuke Hayashi <26004436+hys-rabbit@users.noreply.github.com>
2019-08-29 19:50:15 +0900

    feat: Initial commit.

---
+++ src/main/java/com/github/hysrabbit/agecorrector/AgecorrectorApplication.java
@@ -0,0 +1,13 @@
+package com.github.hysrabbit.agecorrector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgecorrectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgecorrectorApplication.class, args);
+	}
+
+}
